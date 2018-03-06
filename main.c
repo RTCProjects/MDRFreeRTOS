50e564449b2e6a9e96e918204704d3c471d7cce9
Borisis <ibentechi@gmail.com>
2018-03-06 10:52:22 +0300

    CAN1 64MHz setup

--- main.c
+++ main.c
@@ -54,45 +54,72 @@ void Port_Init()
 		
 	
 		//Ports for CAN
-		
-		  PORT_InitStructure.PORT_FUNC  = PORT_FUNC_ALTER;
-			PORT_InitStructure.PORT_Pin   = PORT_Pin_6;
-			PORT_InitStructure.PORT_OE    = PORT_OE_OUT;
-			PORT_InitStructure.PORT_MODE  = PORT_MODE_DIGITAL;
-			PORT_InitStructure.PORT_SPEED = PORT_SPEED_FAST;
-			PORT_Init(MDR_PORTF, &PORT_InitStructure);
+		RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTA, ENABLE);
+	
+		PORT_InitStructure.PORT_FUNC  = PORT_FUNC_ALTER;
+		PORT_InitStructure.PORT_Pin   = PORT_Pin_6;
+		PORT_InitStructure.PORT_OE    = PORT_OE_OUT;
+		PORT_InitStructure.PORT_MODE  = PORT_MODE_DIGITAL;
+		PORT_InitStructure.PORT_SPEED = PORT_SPEED_FAST;
+		PORT_InitStructure.PORT_PD = PORT_PD_DRIVER;
+		PORT_Init(MDR_PORTA, &PORT_InitStructure);
 
-			PORT_InitStructure.PORT_Pin   = PORT_Pin_7;
-			PORT_InitStructure.PORT_OE    = PORT_OE_IN;
-			PORT_Init(MDR_PORTF, &PORT_InitStructure);
-		
+		PORT_InitStructure.PORT_Pin   = PORT_Pin_7;
+		PORT_InitStructure.PORT_OE    = PORT_OE_IN;
+		PORT_Init(MDR_PORTA, &PORT_InitStructure);
+			
 }
 
 void Clock_Init()
 {
+	RST_CLK_DeInit();
+	RST_CLK_PCLKcmd(RST_CLK_PCLK_BKP,ENABLE);
+	
 	RST_CLK_HSEconfig(RST_CLK_HSE_ON);
-	if (RST_CLK_HSEstatus() == SUCCESS)                     /* Good HSE clock */
-	{
-		RST_CLK_CPU_PLLconfig(RST_CLK_CPU_PLLsrcHSEdiv1, 10);
+	
+	if (RST_CLK_HSEstatus() == SUCCESS){
+		RST_CLK_CPU_PLLconfig(RST_CLK_CPU_PLLsrcHSEdiv1, RST_CLK_CPU_PLLmul8);
 		RST_CLK_CPU_PLLcmd(ENABLE);
 		
-			if (RST_CLK_HSEstatus() == SUCCESS)                     /* Good CPU PLL */
-      {
-        
-        RST_CLK_CPUclkPrescaler(RST_CLK_CPUclkDIV1);
-       
+			if (RST_CLK_HSEstatus() == SUCCESS){
+        RST_CLK_CPUclkPrescaler(RST_CLK_CPUclkDIV1);  
         RST_CLK_CPU_PLLuse(ENABLE);
-        
         RST_CLK_CPUclkSelection(RST_CLK_CPUclkCPU_C3);
-        
       }
 	}
+}
+
+ void CAN_PerifInit()
+{
+	CAN_InitTypeDef  sCAN;
+	
+	RST_CLK_PCLKcmd(RST_CLK_PCLK_CAN1,ENABLE);
+	
+	CAN_BRGInit(MDR_CAN1,CAN_HCLKdiv1);
+	  
+	CAN_StructInit (&sCAN);
+	
+	sCAN.CAN_ROP  = DISABLE;
+  sCAN.CAN_SAP  = ENABLE;
+  sCAN.CAN_STM  = DISABLE;
+  sCAN.CAN_ROM  = DISABLE;
+  sCAN.CAN_PSEG = CAN_PSEG_Mul_2TQ;
+  sCAN.CAN_SEG1 = CAN_SEG1_Mul_7TQ;
+  sCAN.CAN_SEG2 = CAN_SEG2_Mul_6TQ;
+  sCAN.CAN_SJW  = CAN_SJW_Mul_4TQ;
+  sCAN.CAN_SB   = CAN_SB_1_SAMPLE;
+  sCAN.CAN_BRP  = 7;
+  CAN_Init (MDR_CAN1,&sCAN);
+  CAN_Cmd(MDR_CAN1, ENABLE);
+
+
 }
 
 int main()
 {
 	Clock_Init();
 	Port_Init();
+	CAN_PerifInit();
 	
 	xTaskCreate(vLed1, "1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
 	xTaskCreate(vLed2, "2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
